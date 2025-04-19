10dd20d4b95d5365f39c33e601c12522ff74580e
Blessing-09 <blessingogbogu@gmail.com>
2025-04-19 04:55:47 +0200

    Updates and debugging

--- src/routes.jsx
+++ src/routes.jsx
@@ -10,6 +10,9 @@ import { Home } from "./pages/Home";
 import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
 import { PersonDetails } from "./pages/PersonDetails";
+import { VehicleDetails } from "./pages/VehicleDetails";
+import { FilmDetails } from "./pages/FilmDetails";
+import { PlanetDetails } from "./pages/PlanetDetails";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -27,6 +30,9 @@ export const router = createBrowserRouter(
         <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
         <Route path="/demo" element={<Demo />} />
         <Route path="/person/:uid" element={<PersonDetails/>} />
+        <Route path="/vehicle/:uid" element={<VehicleDetails/>} />
+        <Route path="/planet/:uid" element={<PlanetDetails/>} />
+        <Route path="/film/:uid" element={<FilmDetails/>} />
       </Route>
     )
 );
